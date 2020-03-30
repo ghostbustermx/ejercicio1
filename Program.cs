e4a98c353179d200492fb6084a0340058626be88
ghostbustermx <50531427+ghostbustermx@users.noreply.github.com>
2020-03-29 20:22:45 -0600

    first commit
    functional

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+
+            for (i = 0; i <= 100; i++)
+                if (i % 3 == 0 && i % 5 == 0 ){
+                    Console.WriteLine("TC");
+                }else if (i % 5 == 0 ){
+                    Console.WriteLine("C");
+                }else if ( i % 3 == 0 ){
+                    Console.WriteLine("T");
+                }else{
+                    Console.Write("{0}|", i);
+                }
+                
+
+                Console.ReadLine();
+            
+            
+        }
+    }
+}
